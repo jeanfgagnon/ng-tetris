67f0cdac79005770dae21b1832ca8f20c3511361
JF Gagnon <lapuff@yahoo.com>
2020-11-08 22:09:26 -0500

    High Score Ok (add)

---
+++ src/app/models/highscore-model.ts
@@ -0,0 +1,7 @@
+export class HighScoreModel {
+  name: string;
+  date: Date;
+  score: number;
+  lines: number;
+  elapsed: number;
+}
---
+++ src/app/models/message-model.ts
@@ -0,0 +1,4 @@
+export class MessageModel {
+  message: string;
+  isEndGame: boolean;
+}
